95539dc4c8e5eab039e231148f4abf889849ddab
Hugo Arregui <hugo@decentraland.org>
2023-04-10 16:57:20 -0300

    Save Mon Apr 10 04:57:20 PM -03 2023

---
+++ generate-apps-workflows.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+function usage {
+  echo -e "usage:
+generate-apps-workflows.sh <name>
+"
+}
+
+if [ $# -eq 0 ]; then
+  usage
+  exit 1
+fi
+
+NAME=$1
+
+mkdir -p $NAME/.github/workflows
+cp apps-template/*.yml $NAME/.github/workflows
+
+sed -i "s/\$SERVICE_NAME/${NAME}/g" $NAME/.github/workflows/*.yml
